a187741e86623b3e8e7ac9a44a4a0d9b74f2c763
Ashwin Balakrishna <abalakrishna@gmail.com>
2019-04-05 02:12:46 -0700

    tiny fix to setup.py and added tqdm

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     version = '0.1.0',
     description = 'Unsupervised Representation Learning by Predicting Rigid Body Transformations in Depth Images',
     author = 'Ashwin Balakrishna, Chris Correa',
-    author_email = '???, chris.correa@berkeley.edu',
+    author_email = 'ashwin_balakrishna@berkeley.edu, chris.correa@berkeley.edu',
     url = 'https://github.com/BerkeleyAutomation/unsupervised_rbt',
     package_dir = {'': '.'},
     packages = ['unsupervised_rbt'],
